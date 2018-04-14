0dd087e57e6e329418f44cec10939f718aab4a47
Alice Wonder <github@domblogger.net>
2018-04-14 01:31:04 -0700

    Planned, not yet written

---
+++ lib/Ogre.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Generates an Ogre avatar SVG file.
+ *
+ * @package AWonderPHP/Groovytar
+ * @author  Alice Wonder <paypal@domblogger.net>
+ * @license https://opensource.org/licenses/MIT MIT
+ * @link    https://github.com/AliceWonderMiscreations/Groovytar
+ */
+
+namespace AWonderPHP\Groovytar;
+
+/**
+ * Ogre Generation
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ lib/Silhouette.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Generates a Silhouette avatar SVG file.
+ *
+ * @package AWonderPHP/Groovytar
+ * @author  Alice Wonder <paypal@domblogger.net>
+ * @license https://opensource.org/licenses/MIT MIT
+ * @link    https://github.com/AliceWonderMiscreations/Groovytar
+ */
+
+namespace AWonderPHP\Groovytar;
+
+/**
+ * Silhouette Generation
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
