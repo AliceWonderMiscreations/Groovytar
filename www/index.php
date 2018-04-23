d9c7c3dc77af759f370d9fa2073410263ac21dc7
Alice Wonder <github@domblogger.net>
2018-04-23 02:49:22 -0700

    add examples

--- www/index.php
+++ www/index.php
@@ -17,5 +17,44 @@ $hash = md5($foo, false);
 $str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=96" style="width: 96px;" />';
 echo($str); 
 ?>
+<p>128 CSS pixels wide:</p>
+<?php
+$str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=128" style="width: 128px;" />';
+echo($str); 
+?>
+<p>192 CSS pixels wide:</p>
+<?php
+$str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=192" style="width: 192px;" />';
+echo($str); 
+?>
+<p>256 CSS pixels wide:</p>
+<?php
+$str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=256" style="width: 256px;" />';
+echo($str); 
+?>
+
+<h2>Confetti</h2>
+<p>The following Confetti identicon is generated from the defined hash, shown displayed at various sizes:</p>
+<p>96 CSS pixels wide:</p>
+<?php
+$str = '<img src="/avatar/' . $hash . '?d=confetti&s=96" style="width: 96px;" />';
+echo($str); 
+?>
+<p>128 CSS pixels wide:</p>
+<?php
+$str = '<img src="/avatar/' . $hash . '?d=confetti&s=128" style="width: 128px;" />';
+echo($str); 
+?>
+<p>192 CSS pixels wide:</p>
+<?php
+$str = '<img src="/avatar/' . $hash . '?d=confetti&s=192" style="width: 192px;" />';
+echo($str); 
+?>
+<p>256 CSS pixels wide:</p>
+<?php
+$str = '<img src="/avatar/' . $hash . '?d=confetti&s=256" style="width: 256px;" />';
+echo($str); 
+?>
+
 </body>
 </html>
