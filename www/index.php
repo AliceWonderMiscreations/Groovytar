82527ea20fed2cd4c318a034a1fc3f267ccb5090
Alice Wonder <github@domblogger.net>
2018-04-23 02:41:04 -0700

    test index

--- www/index.php
+++ www/index.php
@@ -1,4 +1,21 @@
+<html>
+<head>
+<title>Groovytar Identicon Example Page</title>
+</head>
+<body>
+<h1>Groovytar Identicon Example Page</h1>
+<p>Excuse the poor web design, I will make something better later.</p>
 <?php
-// placeholder
-phpinfo();
+$foo = rand_bytes(10);
+$hash = md5($foo, false);
 ?>
+<p>The hash being used for this example: <code><?php echo $hash;?></code></p>
+<h2>PictoGlyph Identicon</h2>
+<p>The following PictoGlyph identicon is generated from the defined hash, shown displayed at various sizes:</p>
+<p>96 CSS pixels wide:</p>
+<?php
+$str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=96" style="width: 96px;" />';
+echo($str); 
+?>
+</body>
+</html>
