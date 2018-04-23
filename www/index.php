9981bf1db53fcc927ea49e10d3f980bd62cb40b4
Alice Wonder <github@domblogger.net>
2018-04-23 11:52:03 -0700

    remove excess whitespace

--- www/index.php
+++ www/index.php
@@ -21,22 +21,22 @@ $hash = md5($foo, false);
 <p>96 CSS pixels wide:</p>
 <?php
 $str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=96" style="width: 96px;" /><hr />';
-echo($str); 
+echo($str);
 ?>
 <p>128 CSS pixels wide:</p>
 <?php
 $str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=128" style="width: 128px;" /><p><a href="/avatar/' . $hash . '?d=pictoglyph&s=128" target="_blank">Link to Full View</a></p><hr />';
-echo($str); 
+echo($str);
 ?>
 <p>192 CSS pixels wide:</p>
 <?php
 $str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=192" style="width: 192px;" /><hr />';
-echo($str); 
+echo($str);
 ?>
 <p>256 CSS pixels wide:</p>
 <?php
 $str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=256" style="width: 256px;" /><p><a href="/avatar/' . $hash . '?d=pictoglyph&s=256" target="_blank">Link to Full View</a></p><hr />';
-echo($str); 
+echo($str);
 ?>
 
 <h2>Confetti</h2>
@@ -45,22 +45,22 @@ echo($str);
 <p>96 CSS pixels wide:</p>
 <?php
 $str = '<img src="/avatar/' . $hash . '?d=confetti&s=96" style="width: 96px;" /><hr />';
-echo($str); 
+echo($str);
 ?>
 <p>128 CSS pixels wide:</p>
 <?php
 $str = '<img src="/avatar/' . $hash . '?d=confetti&s=128" style="width: 128px;" /><hr />';
-echo($str); 
+echo($str);
 ?>
 <p>192 CSS pixels wide:</p>
 <?php
 $str = '<img src="/avatar/' . $hash . '?d=confetti&s=192" style="width: 192px;" /><hr />';
-echo($str); 
+echo($str);
 ?>
 <p>256 CSS pixels wide:</p>
 <?php
 $str = '<img src="/avatar/' . $hash . '?d=confetti&s=256" style="width: 256px;" /><p><a href="/avatar/' . $hash . '?d=confetti&s=256" target="_blank">Link to Full View</a></p><hr />';
-echo($str); 
+echo($str);
 ?>
 
 </body>
