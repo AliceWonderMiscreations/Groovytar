376db50c7b8e8e54b90cfc845d19a2c0de0e96b2
Alice Wonder <github@domblogger.net>
2018-04-23 03:10:17 -0700

    update index

--- www/index.php
+++ www/index.php
@@ -11,48 +11,51 @@ $hash = md5($foo, false);
 ?>
 <p>The hash being used for this example: <code><?php echo $hash;?></code></p>
 <h2>PictoGlyph Identicon</h2>
+<p>This identicon is not yet finished, right now only ten different glyphs have been generated (there will be 32) and only about 60 color combinations have chosen, some of which will be rejected (there will be 128).</p>
+<p>For small display, there are three rows of three. For large display, there are four rows of four.</p>
 <p>The following PictoGlyph identicon is generated from the defined hash, shown displayed at various sizes:</p>
 <p>96 CSS pixels wide:</p>
 <?php
-$str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=96" style="width: 96px;" />';
+$str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=96" style="width: 96px;" /><hr />';
 echo($str); 
 ?>
 <p>128 CSS pixels wide:</p>
 <?php
-$str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=128" style="width: 128px;" />';
+$str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=128" style="width: 128px;" /><p><a href="/avatar/' . $hash . '?d=pictoglyph&s=128" target="_blank">Link to Full View</a></p><hr />';
 echo($str); 
 ?>
 <p>192 CSS pixels wide:</p>
 <?php
-$str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=192" style="width: 192px;" />';
+$str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=192" style="width: 192px;" /><hr />';
 echo($str); 
 ?>
 <p>256 CSS pixels wide:</p>
 <?php
-$str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=256" style="width: 256px;" />';
+$str = '<img src="/avatar/' . $hash . '?d=pictoglyph&s=256" style="width: 256px;" /><p><a href="/avatar/' . $hash . '?d=pictoglyph&s=256" target="_blank">Link to Full View</a></p><hr />';
 echo($str); 
 ?>
 
 <h2>Confetti</h2>
+<p>Other than maybe improving the generated frame, this one is done. The same image is used large or small.</p>
 <p>The following Confetti identicon is generated from the defined hash, shown displayed at various sizes:</p>
 <p>96 CSS pixels wide:</p>
 <?php
-$str = '<img src="/avatar/' . $hash . '?d=confetti&s=96" style="width: 96px;" />';
+$str = '<img src="/avatar/' . $hash . '?d=confetti&s=96" style="width: 96px;" /><hr />';
 echo($str); 
 ?>
 <p>128 CSS pixels wide:</p>
 <?php
-$str = '<img src="/avatar/' . $hash . '?d=confetti&s=128" style="width: 128px;" />';
+$str = '<img src="/avatar/' . $hash . '?d=confetti&s=128" style="width: 128px;" /><hr />';
 echo($str); 
 ?>
 <p>192 CSS pixels wide:</p>
 <?php
-$str = '<img src="/avatar/' . $hash . '?d=confetti&s=192" style="width: 192px;" />';
+$str = '<img src="/avatar/' . $hash . '?d=confetti&s=192" style="width: 192px;" /><hr />';
 echo($str); 
 ?>
 <p>256 CSS pixels wide:</p>
 <?php
-$str = '<img src="/avatar/' . $hash . '?d=confetti&s=256" style="width: 256px;" />';
+$str = '<img src="/avatar/' . $hash . '?d=confetti&s=256" style="width: 256px;" /><p><a href="/avatar/' . $hash . '?d=confetti&s=256" target="_blank">Link to Full View</a></p><hr />';
 echo($str); 
 ?>
 
